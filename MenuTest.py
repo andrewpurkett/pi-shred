f563d1813e70ca8bcdbff7a579adae21fed1bf92
Andrew <andrewpurkett@users.noreply.github.com>
2014-06-25 18:30:43 -0600

    Create MenuTest.py

---
+++ MenuTest.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+from time import sleep
+import pifacecad
+
+UPDATE_INTERVAL = 15  # 15 seconds
+
+def run_cmd(cmd):
+    return subprocess.check_output(cmd, shell=True).decode('utf-8')
+
+
+def show_sysinfo():
+    while True:
+        cad.lcd.clear()
+        cad.lcd.write("1) SCAN 2) WIPE\n3)SHRED 4) FIND")
+
+        sleep(UPDATE_INTERVAL)
+
+
+if __name__ == "__main__":
+    cad = pifacecad.PiFaceCAD()
+    cad.lcd.blink_off()
+    cad.lcd.cursor_off()
+
+    if "clear" in sys.argv:
+        cad.lcd.clear()
+        cad.lcd.display_off()
+        cad.lcd.backlight_off()
+    else:
+        cad.lcd.clear()
+        cad.lcd.backlight_on()
+        cad.lcd.write("PI-SHRED v0.01\nwww.aj.cm/pi")
+        show_sysinfo()
